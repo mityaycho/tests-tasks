a1ba96776742a03260c7c73b41ad586abcbb327d
mityaycho <mityaycho@gmail.com>
2024-01-29 15:16:06 +0300

    add date

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -2,8 +2,11 @@
 
 export const Clock = () => {
 
+	const date = new Date();
+
 	return (
 		<>
+		{`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`}
 		<button>analog clock</button>
 		<button>digital clock</button>
 		</>
